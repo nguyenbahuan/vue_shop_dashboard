f96dc9652a7570a65ba6e1a3623099740ac6e5eb
soojaa202 <90172139+nguyenbahuan@users.noreply.github.com>
2024-05-08 17:32:01 +0700

    update code

--- vue.config.js
+++ vue.config.js
@@ -4,8 +4,4 @@ module.exports = defineConfig({
   devServer: {
     port: 3000,
   },
-  publicPath:
-    process.env.NODE_ENV === "production"
-      ? "/shop-huannb/" // Thay tên repository của các bạn vào đây nhé
-      : "/",
 });
